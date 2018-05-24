e95851c07fe30a9c3faa0d937733a59aeadafb86
Elynace07 <dominicangirl0510@gamil.com>
2018-05-24 12:51:03 -0400

    Edit final dfraft of portfolio

---
+++ finalgrade.js
@@ -0,0 +1,165 @@
+// Example 1:
+// ● Write an algorithm to determine a student’s final grade and indicate
+// whether it is passing or failing
+// ● The final grade is calculated as the average of four marks
+
+// step 1 input
+
+// -we will input 4 numbers of grades
+ 
+//  step 2 process
+//  we will take the 4 numbers and add them
+//  we will store the sum in a variable
+//  Divide the sum by 4 
+//  store the final score
+
+//  -step 3 output
+//  if the final score is  above 65% let them know they passed
+//  if the final score is below 65% they failed
+
+
+// start of my algorithm
+
+// step 1: [input]
+	
+// 	- we need to enter two values
+// 	- lets call them valueOne and valueTwp
+
+// step 2: [process]
+
+// 	-create and add a variable called MaxNumber
+	
+// 	-store the values in a variable sp we can use it later in our code
+    
+//     -Compare the two values to each other
+    
+//     -if valueOne is greater than valueTwo
+    
+//     -Then valueOne is the newMaxNumber
+    
+//     -else valueTwo is the new MaxNumber
+
+// step 3: [output]
+	
+// 	- Log out to the screen this is the MaxNumber
+
+// 	End of my algorithm
+
+function LargestValue(valueOne, ValueTwo){
+
+	var MaxNumber;
+
+	if (ValueOne > ValueTwo) {
+		MaxNumber = ValueOne;
+	} else {
+	MaxNumber = valueTwo;	
+	} 
+
+	console.log('Here is the largest number' + MaxNumber);
+
+}
+
+
+// ● Example 3:
+// ● Write an algorithm that reads three numbers and prints the value of
+// // the largest number
+// 	step 1:  Input
+// 	-Input 	3 values
+// 	-Lets call them ValueOne, ValueTwo, ValueThree
+
+// 	Step 2: process
+// 	-Create a variable called BiggestNumber
+// 	-Compare the three values
+// 	-If ValueOne is greater than valueTwo and valueThree
+// 	-then valueOne is the new BiggestNumber
+// 	-else if valueTwo is greater than valueOne and valueThree 
+// 	- than ValueTwo is the new BiggestNumber
+// 	-BiggestNumber is ValueThree
+
+// 	Step 3: output
+
+// 	 End of my algorithm
+
+
+
+
+
+step 1:  Input
+// 	-Input 	3 values
+// 	- While number given is less than ten
+
+// 	Step 2: process
+// 	-Ask for a number greater then ten
+
+// 	Step 3: output
+	-	end while
+// 	 End of my algorithm
+
+
+
+
+
+
+
+// To print numbers from 10 to 20
+// Step 1: Input 
+// -input number 10
+var counter = 10;
+// Step 2: process
+// - Count 10 to 20
+
+ while (counter <= 20n {
+
+// Step 3: Output
+
+ console.log ( 'This is the counter ===> ' +counter')
+ }
+
+// - Print out numbers
+ 
+//  End of My algorithm
+
+
+
+
+
+
+
+<!DOCTYPE html
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
+
+
+
+
+ 
+
+
---
+++ script.js
@@ -0,0 +1,3 @@
+// var user = prompt	("Enter your name") ;
+var  myPIZZA = document.getElementByID(myPizza);
+myPIZZA.innerHTML = "Give me pizza"
